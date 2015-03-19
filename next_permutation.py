5e326c9bc91217f394e6d5b444512a2531b985e4
李泳 <yong.li1992@foxmail.com>
2015-03-19 07:41:06 +0000

    7:41 19/3/15

--- next_permutation.py
+++ next_permutation.py
@@ -10,31 +10,66 @@
 # 3,2,1 → 1,2,3
 # 1,1,5 → 1,5,1
 
+import itertools
+
 class Solution:
+    def nextPermutation(self, num):
+        str_digit_list = [str(n) for n in num]
+        permu_list = list(itertools.permutations(str_digit_list))
+        num_list = sorted([int("".join(t)) for t in permu_list])
+        tuple_list = [tuple(self.split_num(n)) for n in num_list]
+
+        index = tuple_list.index(tuple(num))
+        num[:] = []
+        if index == len(tuple_list) - 1:
+            num.extend(tuple_list[0])
+        else:
+            num.extend(tuple_list[index+1])
+
+    def split_num(self, num):
+        digit_list = []
+        while num / 10:
+            digit_list.append(num % 10)
+            num /= 10
+        digit_list.append(num % 10)
+        digit_list.reverse()
+        return digit_list
+
+class Solution2:
     # @paramnum, a list of integer
     # @erturn nothing (void), do not return anything, modify num in-place instead.
     def nextPermutation(self, num):
-        num_index_list = []
-        for i,n in enumerate(num):
-            num_index_list.append((n,i))
-
-        num_index_list.sort(key=lambda t:t[0])
-        print num_index_list
-
-        for index,ni_tuple in enumerate(num_index_list):
-            n = ni_tuple[0]
-            i = ni_tuple[1]
-            if i == index:
-                # Number in the largest possible place
+        max_n = -float("inf")
+        max_i = -1
+        for i in range(len(num)-1, -1, -1):
+            n = num[i]
+            if n >= max_n:
+                max_n = n
+                max_i = i
                 continue
             else:
-                # Number not in the largest possible place
-                num[i-1], num[i] = num[i], num[i-1]
+                # This num is not the largest num we've met, find the one slightly larger than it on its right side
+                larger_index = -1
+                larger_num = -float("inf")
+                for j in range(len(num)-1, i, -1):
+                    if num[j] > n:
+                        larger_index = j
+                        larger_num = num[j]
+                        break
+                num[i+1:larger_index+1] = num[i:larger_index]
+                num[i] = larger_num
+                num[i+1:] = sorted(num[i+1:])
+
+                # We are done
                 break
         else:
-            # All numbers in the largest possible place
             num.reverse()
 
-num = [1,3,2]
-Solution().nextPermutation(num)
+
+
+# num = [1,3,2]
+# Solution().nextPermutation(num)
+# print num
+num = [2,3,1]
+Solution2().nextPermutation(num)
 print num
